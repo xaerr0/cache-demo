d406ecba7251b7d85f0da1535ef23ad660cb9700
Danny Brown <xaerr00@gmail.com>
2023-02-26 22:33:28 -0800

    First Commit

---
+++ src/main/java/codingnomads/com/example/cache/demo/CacheDemoApplication.java
@@ -0,0 +1,13 @@
+package codingnomads.com.example.cache.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheDemoApplication.class, args);
+	}
+
+}
